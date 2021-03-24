1b58a3dd7e6ff2bf042fbc66d026dc7a048f48cd
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-03-24 16:57:29 +0530

    chore: first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.listen(PORT);
+
+console.log('Listening on port: ', PORT);
+
+
+

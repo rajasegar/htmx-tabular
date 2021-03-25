26926b33fa36e2bc742c0a3f6b5bfacf1118cb25
Rajasegar <rajasegar.c@gmail.com>
2021-03-25 11:28:50 +0530

    chore: data, sort and pagination

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const pug = require('pug');
 const bodyParser = require('body-parser');
+const superfood = require('superfood');
+const R = require('ramda');
 
 
 const PORT = process.env.PORT || 3000;
@@ -11,9 +13,32 @@ app.use(express.static('assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const items = new Array(100);
+const restaurants = [...items].map(x => {
+  return {
+    name: superfood.random(),
+    stars: 4,
+    price: '$$',
+    category: 'Greek'
+  };
+});
+
+console.log(restaurants.length);
+
+const totalPages = restaurants.length / 10;
 
 app.get('/', (req, res) => {
-  res.render('index');
+  let { start, sortBy, direction } = req.query;
+  start = start || 0;
+  sortBy = sortBy || 'name';
+  direction = direction || 'asc';
+  const reverse = direction === 'asc' ? 'desc' : 'asc';
+  const sortedData = direction === 'asc'
+    ? R.sortWith([R.ascend(R.prop(sortBy))], restaurants)
+    : R.sortWith([R.descend(R.prop(sortBy))], restaurants);
+
+  const data = sortedData.slice(start, Number(start) + 10);
+  res.render('index', { restaurants: data, start, totalPages, reverse, direction, sortBy });
 });
 
 
